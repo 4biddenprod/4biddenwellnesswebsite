9f1ee3c5ff2ae4d5ac25df481c9240b54d616759
4biddenprod <frederickdouglass619@gmail.com>
2025-02-07 15:41:06 +0100

    Auto

---
+++ assets/js/crisp.js
@@ -0,0 +1,9 @@
+window.$crisp = [];
+window.CRISP_WEBSITE_ID = "50561097-fb4b-4eea-bd2a-68f5f1bd8c4d";
+(function () {
+    var d = document;
+    var s = d.createElement("script");
+    s.src = "https://client.crisp.chat/l.js";
+    s.async = 1;
+    d.getElementsByTagName("body")[0].appendChild(s);
+})();
