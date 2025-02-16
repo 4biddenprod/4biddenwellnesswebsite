00c6bd3f7f485403280fe77e7a388077e66746ca
4biddenprod <frederickdouglass619@gmail.com>
2025-02-16 23:53:54 +0100

    Auto

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -574,6 +574,6 @@ function ml_webform_success_22728804() {
     $('.ml-subscribe-form-22728804 .row-form').hide();
   }
 
-<script src="https://groot.mailerlite.com/js/w/webforms.min.js?v176e10baa5e7ed80d35ae235be3d5024" type="text/javascript"></script>
+src="https://groot.mailerlite.com/js/w/webforms.min.js?v176e10baa5e7ed80d35ae235be3d5024"
 
   fetch("https://assets.mailerlite.com/jsonp/1260530/forms/146427399209223985/takel")
